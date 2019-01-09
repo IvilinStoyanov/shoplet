bd2c82b157008b68985a61a365b9fcaa2c059fc0
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2019-01-09 23:57:53 +0200

    add shopletUser

---
+++ src/Data/shoplet.Data.Models/shopletUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace shoplet.Web.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the shopletUser class
+    public class ShopletUser : IdentityUser
+    {
+        public string Address { get; set; }
+    }
+}
